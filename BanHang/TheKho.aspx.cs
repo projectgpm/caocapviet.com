005f77e6cb91e740cbf5ac1c2bcf92b058749cdf
huynhvuluangpm <luanhv@gpm.vn>
2017-09-11 12:09:55 +0700

    33333

---
+++ BanHang/TheKho.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace BanHang
+{
+    public partial class TheKho : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnLoc_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
