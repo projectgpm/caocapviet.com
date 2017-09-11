1fd99d14041beb65780d6c82f6617e7923e6dec0
huynhvuluangpm <luanhv@gpm.vn>
2017-09-11 17:44:46 +0700

    4234

--- BanHang/Data/dtMasterPage.cs
+++ BanHang/Data/dtMasterPage.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
@@ -7,6 +9,55 @@ namespace BanHang.Data
 {
     public class dtMasterPage
     {
-
+        public DataTable DanhSachDonHangChiNhanhChuaXuLy(DateTime NgayHomNay)
+        {
+            using (SqlConnection con = new SqlConnection(StaticContext.ConnectionString))
+            {
+                con.Open();
+                string cmdText = "SELECT ID,SoDonHang,IDKhoLap FROM [GPM_DuyetHangChiNhanh] WHERE [GPM_DuyetHangChiNhanh].IDTrangThaiXuLy != 2 AND [GPM_DuyetHangChiNhanh].TrangThaiDuyet = 0 AND [NgayDuyet] < '" + NgayHomNay.ToString("yyyy-MM-dd") + "'";
+                using (SqlCommand command = new SqlCommand(cmdText, con))
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    DataTable tb = new DataTable();
+                    tb.Load(reader);
+                    return tb;
+                }
+            }
+        }
+        public DataTable DanhSachChiTietDuyet(string ID)
+        {
+            using (SqlConnection con = new SqlConnection(StaticContext.ConnectionString))
+            {
+                con.Open();
+                string cmdText = "SELECT * FROM [GPM_DuyetHangChiNhanh_ChiTiet] WHERE IDDuyetHangChiNhanh = '" + ID + "'";
+                using (SqlCommand command = new SqlCommand(cmdText, con))
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    DataTable tb = new DataTable();
+                    tb.Load(reader);
+                    return tb;
+                }
+            }
+        }
+        public void CapNhatDonHangHoanTat(string IDDonHang)
+        {
+            using (SqlConnection myConnection = new SqlConnection(StaticContext.ConnectionString))
+            {
+                try
+                {
+                    myConnection.Open();
+                    string cmdText = "UPDATE [GPM_DuyetHangChiNhanh] SET [TrangThaiDuyet] = 1 WHERE [ID] = '" + IDDonHang + "'";
+                    using (SqlCommand myCommand = new SqlCommand(cmdText, myConnection))
+                    {
+                        myCommand.ExecuteNonQuery();
+                    }
+                    myConnection.Close();
+                }
+                catch
+                {
+                    throw new Exception("Lỗi: Quá trình thêm dữ liệu gặp lỗi");
+                }
+            }
+        }
     }
 }
--- BanHang/Root.master.cs
+++ BanHang/Root.master.cs
@@ -11,6 +11,7 @@ using System.Data;
 
 namespace BanHang {
     public partial class RootMaster : System.Web.UI.MasterPage {
+        dtMasterPage data = new dtMasterPage();
         protected void Page_Load(object sender, EventArgs e) {
             if (Session["KTDangNhap"] != "GPM")
             {
@@ -18,13 +19,40 @@ namespace BanHang {
             }
             else
             {
+                XuLyDonHangChiNhanh();
                 lblChao.Text = "Xin Chào: " + Session["TenDangNhap"].ToString();
                 ASPxLabel2.Text = Server.HtmlDecode("Copyrights &copy;") + DateTime.Now.Year + Server.HtmlDecode(". All Rights Reserved. Designed by GPM.VN");
             }
         }
         public void XuLyDonHangChiNhanh()
         {
-
+            data = new dtMasterPage();
+            DataTable db = data.DanhSachDonHangChiNhanhChuaXuLy(DateTime.Now);
+            if (db.Rows.Count > 0)
+            {
+                foreach (DataRow dr in db.Rows)
+                {
+                    string ID = dr["ID"].ToString();
+                    string IDKho = dr["IDKhoLap"].ToString();
+                    if (ID != "")
+                    {
+                      
+                        DataTable dt = data.DanhSachChiTietDuyet(ID);
+                        foreach (DataRow dr1 in dt.Rows)
+                        {
+                            string IDHangHoa = dr1["IDHangHoa"].ToString();
+                            int SoLuong = Int32.Parse(dr1["ThucTe"].ToString());
+                            object TheKho = dtTheKho.ThemTheKho(dtDuyetDonHangChiNhanh.LaySoDonHang(ID), "Xác nhận đơn hàng tự động ", SoLuong.ToString(), "0", (Int32.Parse(dtCapNhatTonKho.SoLuong_TonKho(IDHangHoa, IDKho).ToString()) + SoLuong).ToString(), "1", IDKho, IDHangHoa, "Nhập");
+                            if (TheKho != null)
+                            {
+                                dtCapNhatTonKho.CongTonKho(IDHangHoa, SoLuong.ToString(), IDKho);
+                            }
+                        }
+                        data = new dtMasterPage();
+                        data.CapNhatDonHangHoanTat(ID);
+                    }
+                }
+            }
         }
     }
 }
