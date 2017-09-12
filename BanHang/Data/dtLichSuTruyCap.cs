dc0a81280bf0fe291a6c411e6f9e6ccfd33bb99a
huynhvuluangpm <luanhv@gpm.vn>
2017-09-12 17:28:06 +0700

    123123

--- BanHang/Data/dtLichSuTruyCap.cs
+++ BanHang/Data/dtLichSuTruyCap.cs
@@ -14,7 +14,7 @@ namespace BanHang.Data
             using (SqlConnection con = new SqlConnection(StaticContext.ConnectionString))
             {
                 con.Open();
-                string cmdText = "SELECT * FROM [GPM_LichSuTruyCap] ORDER BY [ID] DESC ";
+                string cmdText = "SELECT TOP 1000 * FROM [GPM_LichSuTruyCap] ORDER BY [ID] DESC ";
                 using (SqlCommand command = new SqlCommand(cmdText, con))
                 using (SqlDataReader reader = command.ExecuteReader())
                 {
